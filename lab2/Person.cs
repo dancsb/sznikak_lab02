7db6ec94eb5caac817958906e99da7e58d08bf9a
User <User@DESKTOP-J6E2U7E>
2022-03-09 14:43:16 +0100

    Add project files.

---
+++ lab2/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2
+{
+    public class Person
+    {
+        public AgeChangingDelegate AgeChanging;
+
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0)
+                    throw new Exception("AAAAAAAAAAA");
+                /*
+                if (AgeChanging != null)
+                    AgeChanging(age, value);
+                */
+                AgeChanging?.Invoke(age, value);
+                age = value; }
+        }
+
+        public string Name { get; set; }
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab2
+{
+
+    public delegate void AgeChangingDelegate(int oldAge, int newAge);
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+
+            p.Age = 17;
+            p.Age++;
+            p.Name = "Luke";
+
+            Console.WriteLine(p.Age);
+            Console.WriteLine(p.Name);
+
+            //p.Age = -3;
+        }
+    }
+}
